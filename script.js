e82359c0808407af989bba88ea6fdf78ffccac42
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-03-21 13:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 window.promises = [];
-
 // Do not change the code above this
 // add your promises to the array `promises`
+let random = Math.floor(Math.random()*6)
+
+let promise1 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("1");
+  },random * 1000)
+})
+
+let promise2 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("2");
+  },random * 1000)
+})
+
+let promise3 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("3");
+  },random * 1000)
+})
+
+let promise4 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("4");
+  },random * 1000)
+})
+
+let promise5 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("5");
+  },random * 1000)
+})
+
+promises =[promise1,promise2,promise3,promise4,promise5]
+Promise.any(promises).then(data=>{
+  document.getElementById("output").innerHTML = data;
+})
